a10a61f283e0e834c4056927c2cf93f4e099116e
sakir101 <sakirhossainf99@gmail.com>
2022-08-23 04:16:39 +0600

    total calculation done

--- price.js
+++ price.js
@@ -7,29 +7,45 @@ const totalPrice = document.getElementById('total-price');
 let playerPriceValue;
 let totalPlayerPrice;
 
- document.getElementById('player-price-cal').addEventListener('click',function(){
-let playerPriceValue = parseInt (playerPrice.value);
-    if(playerPrice.value == ""){
+document.getElementById('player-price-cal').addEventListener('click', function () {
+    playerPriceValue = parseInt(playerPrice.value);
+    if (playerPrice.value == "") {
         alert("Input Number");
-        playerPrice.value ="";
+        playerPrice.value = "";
+        return;
     }
 
-    else{
+    else {
         totalPlayerPrice = playerPriceValue * 5;
         playerExpense.innerText = totalPlayerPrice;
     }
-    
- })
 
- document.getElementById('total-cal').addEventListener('click', function(){
-    let managerPriceValue = parseInt (managerPrice.value);
-    let coachPriceValue = parseInt (coachPrice.value);
+})
 
-    if(playerPrice.value == "" || managerPrice.value == "" || coachPrice.value == ""){
+document.getElementById('total-cal').addEventListener('click', function () {
+    let managerPriceValue = parseInt(managerPrice.value);
+    let coachPriceValue = parseInt(coachPrice.value);
+
+    if (playerPrice.value == "" || managerPrice.value == "" || coachPrice.value == "") {
         alert("Input Number");
-        playerPrice.value ="";
+        playerPrice.value = "";
         managerPrice.value = "";
         coachPrice.value = "";
+        return;
     }
 
- })
+    else {
+        if (typeof (totalPlayerPrice) == 'undefined') {
+            alert("Please calculculate per player price first");
+            return;
+        }
+
+        else {
+            let totalPriceValue = managerPriceValue + coachPriceValue + totalPlayerPrice;
+            totalPrice.innerText = totalPriceValue;
+        }
+
+
+    }
+
+})
