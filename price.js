2423fe71db9fc59d19f7f68c02e153bcff1e0154
sakir101 <sakirhossainf99@gmail.com>
2022-08-23 03:35:12 +0600

    other price condition done

--- price.js
+++ price.js
@@ -1,24 +1,35 @@
 const playerPrice = document.getElementById('player-price');
-
 const playerExpense = document.getElementById('player-expense');
-
-
 const managerPrice = document.getElementById('manager-price');
-let managerPriceValue = parseInt (managerPrice.value);
 const coachPrice = document.getElementById('coach-price');
-let coachPriceValue = parseInt (coachPrice.value);
- const totalPrice = document.getElementById('total-price');
- let totalPriceValue = totalPrice.innerText;
+const totalPrice = document.getElementById('total-price');
+
+let playerPriceValue;
+let totalPlayerPrice;
 
  document.getElementById('player-price-cal').addEventListener('click',function(){
-    let playerPriceValue = parseInt (playerPrice.value);
+let playerPriceValue = parseInt (playerPrice.value);
     if(playerPrice.value == ""){
         alert("Input Number");
+        playerPrice.value ="";
     }
 
     else{
-        const totalPlayerPrice = playerPriceValue * 5;
+        totalPlayerPrice = playerPriceValue * 5;
         playerExpense.innerText = totalPlayerPrice;
     }
     
+ })
+
+ document.getElementById('total-cal').addEventListener('click', function(){
+    let managerPriceValue = parseInt (managerPrice.value);
+    let coachPriceValue = parseInt (coachPrice.value);
+
+    if(playerPrice.value == "" || managerPrice.value == "" || coachPrice.value == ""){
+        alert("Input Number");
+        playerPrice.value ="";
+        managerPrice.value = "";
+        coachPrice.value = "";
+    }
+
  })
