57c3d56bb57f5dbfb94f17a9284bc622aaf28b32
sakir101 <sakirhossainf99@gmail.com>
2022-08-23 15:21:55 +0600

    formate change

--- player.js
+++ player.js
@@ -24,112 +24,112 @@ function playerNumMax() {
     alert("Choose Only Five Players")
 }
 
-function btnDisable(btn){
+function btnDisable(btn) {
     btn.setAttribute('disabled', true);
     btn.style.backgroundColor = 'gray';
 }
 
-    document.getElementById('player-one-btn').addEventListener('click', function (event) {
-        
-        if (playerList.childElementCount < 5) {
-            const li = document.createElement('li');
-            li.innerText = playerOneName;
-            playerList.appendChild(li);
-            btnDisable(event.target);
-            
-        }
-        else {
-            playerNumMax();
-        }  
-    })
-    document.getElementById('player-two-btn').addEventListener('click', function (event) {
-        if (playerList.childElementCount < 5) {
-            const li = document.createElement('li');
-            li.innerText = playerTwoName;
-            playerList.appendChild(li);
-            btnDisable(event.target);
-        }
-        else {
-            playerNumMax();
-        }  
-    })
-    document.getElementById('player-three-btn').addEventListener('click', function (event) {
-        if (playerList.childElementCount < 5) {
-            const li = document.createElement('li');
-            li.innerText = playerThreeName;
-            playerList.appendChild(li);
-            btnDisable(event.target);
-        }
-        else {
-            playerNumMax();
-        }  
-    })
-    document.getElementById('player-four-btn').addEventListener('click', function (event) {
-        if (playerList.childElementCount < 5) {
-            const li = document.createElement('li');
-            li.innerText = playerFourName;
-            playerList.appendChild(li);
-            btnDisable(event.target);
-        }
-        else {
-            playerNumMax();
-        }  
-    })
-    document.getElementById('player-five-btn').addEventListener('click', function (event) {
-        if (playerList.childElementCount < 5) {
-            const li = document.createElement('li');
-            li.innerText = playeFiveName;
-            playerList.appendChild(li);
-            btnDisable(event.target);
-        }
-        else {
-            playerNumMax();
-        }  
-    })
-    document.getElementById('player-six-btn').addEventListener('click', function (event) {
-        if (playerList.childElementCount < 5) {
-            const li = document.createElement('li');
-            li.innerText = playerSixName;
-            playerList.appendChild(li);
-            btnDisable(event.target);
-        }
-        else {
-            playerNumMax();
-        }  
-    })
-    document.getElementById('player-seven-btn').addEventListener('click', function (event) {
-        if (playerList.childElementCount < 5) {
-            const li = document.createElement('li');
-            li.innerText = playerSevenName;
-            playerList.appendChild(li);
-            btnDisable(event.target);
-        }
-        else {
-            playerNumMax();
-        }  
-    })
-    document.getElementById('player-eight-btn').addEventListener('click', function (event) {
-        if (playerList.childElementCount < 5) {
-            const li = document.createElement('li');
-            li.innerText = playerEightName;
-            playerList.appendChild(li);
-            btnDisable(event.target);
-        }
-        else {
-            playerNumMax();
-        }  
-    })
-    document.getElementById('player-nine-btn').addEventListener('click', function (event) {
-        if (playerList.childElementCount < 5) {
-            const li = document.createElement('li');
-            li.innerText = playerNineName;
-            playerList.appendChild(li);
-            btnDisable(event.target);
-        }
-        else {
-            playerNumMax();
-        }  
-    })
+document.getElementById('player-one-btn').addEventListener('click', function (event) {
+
+    if (playerList.childElementCount < 5) {
+        const li = document.createElement('li');
+        li.innerText = playerOneName;
+        playerList.appendChild(li);
+        btnDisable(event.target);
+
+    }
+    else {
+        playerNumMax();
+    }
+})
+document.getElementById('player-two-btn').addEventListener('click', function (event) {
+    if (playerList.childElementCount < 5) {
+        const li = document.createElement('li');
+        li.innerText = playerTwoName;
+        playerList.appendChild(li);
+        btnDisable(event.target);
+    }
+    else {
+        playerNumMax();
+    }
+})
+document.getElementById('player-three-btn').addEventListener('click', function (event) {
+    if (playerList.childElementCount < 5) {
+        const li = document.createElement('li');
+        li.innerText = playerThreeName;
+        playerList.appendChild(li);
+        btnDisable(event.target);
+    }
+    else {
+        playerNumMax();
+    }
+})
+document.getElementById('player-four-btn').addEventListener('click', function (event) {
+    if (playerList.childElementCount < 5) {
+        const li = document.createElement('li');
+        li.innerText = playerFourName;
+        playerList.appendChild(li);
+        btnDisable(event.target);
+    }
+    else {
+        playerNumMax();
+    }
+})
+document.getElementById('player-five-btn').addEventListener('click', function (event) {
+    if (playerList.childElementCount < 5) {
+        const li = document.createElement('li');
+        li.innerText = playeFiveName;
+        playerList.appendChild(li);
+        btnDisable(event.target);
+    }
+    else {
+        playerNumMax();
+    }
+})
+document.getElementById('player-six-btn').addEventListener('click', function (event) {
+    if (playerList.childElementCount < 5) {
+        const li = document.createElement('li');
+        li.innerText = playerSixName;
+        playerList.appendChild(li);
+        btnDisable(event.target);
+    }
+    else {
+        playerNumMax();
+    }
+})
+document.getElementById('player-seven-btn').addEventListener('click', function (event) {
+    if (playerList.childElementCount < 5) {
+        const li = document.createElement('li');
+        li.innerText = playerSevenName;
+        playerList.appendChild(li);
+        btnDisable(event.target);
+    }
+    else {
+        playerNumMax();
+    }
+})
+document.getElementById('player-eight-btn').addEventListener('click', function (event) {
+    if (playerList.childElementCount < 5) {
+        const li = document.createElement('li');
+        li.innerText = playerEightName;
+        playerList.appendChild(li);
+        btnDisable(event.target);
+    }
+    else {
+        playerNumMax();
+    }
+})
+document.getElementById('player-nine-btn').addEventListener('click', function (event) {
+    if (playerList.childElementCount < 5) {
+        const li = document.createElement('li');
+        li.innerText = playerNineName;
+        playerList.appendChild(li);
+        btnDisable(event.target);
+    }
+    else {
+        playerNumMax();
+    }
+})
 
 
 
